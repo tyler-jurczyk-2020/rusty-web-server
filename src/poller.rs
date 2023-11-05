bbae20ca0d63dd82fc091062f4b9ea106b2f134a
tyler-jurczyk-2020 <tylerjurczyk2020@gmail.com>
2023-11-05 00:46:55 -0500

    Restructure tokens to be wrapped in enum

--- src/poller.rs
+++ src/poller.rs
@@ -1,19 +1,27 @@
 use std::error::Error;
-use mio::event::Event;
 use mio::event::Iter;
 use mio::{Interest, Poll, Events, Token};
 use mio::net::{TcpListener, TcpStream};
 use std::net::SocketAddr;
 
+pub enum ConnType {
+    Server,
+    Client(usize)
+}
+
+impl Into<usize> for ConnType {
+    fn into(self) -> usize {
+        match self {
+            ConnType::Server => 0,
+            ConnType::Client(identifier) => identifier
+        }
+    } 
+}
+
 pub struct IO_Handler {
     poll : Poll,
     events : Events,
     pub server : TcpListener
-}
-
-pub enum ConnType {
-    Server,
-    Client
 }
 
 impl IO_Handler {
@@ -35,7 +43,7 @@ pub fn initialize_poll() -> Result<IO_Handler, Box<dyn Error>> {
     let poll = Poll::new()?;
     let events = Events::with_capacity(10);
     let mut server = TcpListener::bind("127.0.0.1:7878".parse()?)?;
-    poll.registry().register(&mut server, Token(0), Interest::READABLE)?;
+    poll.registry().register(&mut server, Token(ConnType::Server.into()), Interest::READABLE)?;
     Ok(IO_Handler { poll, events, server })
 }
 
